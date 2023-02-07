f3299aad677b28b3bb31e64aa4d3ddbaaadc67e9
rzadp <roopert7@gmail.com>
2023-02-07 12:23:01 +0100

    Add example with google auth

--- versioned_docs/version-4.0.0-alpha.1/testnet/Faucet.tsx
+++ versioned_docs/version-4.0.0-alpha.1/testnet/Faucet.tsx
@@ -1,8 +1,10 @@
 import React, {useState} from "react";
 import ReCAPTCHA from "react-google-recaptcha";
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 
 export const Faucet = () => {
   const [captcha, setCaptcha] = useState<string | null>(null)
+  const [googleAuthToken, setGoogleAuthToken] = useState<string | null>(null)
   const [address, setAddress] = useState<string>("")
   const [result, setResult] = useState<string>("")
 
@@ -12,7 +14,8 @@ export const Faucet = () => {
       const body = {
         address,
         parachain_id: "1002",
-        recaptcha: captcha
+        recaptcha: captcha,
+        google_auth_token: googleAuthToken
       }
 
       const fetchResult = await fetch("http://localhost:5555/drip", {
@@ -31,17 +34,19 @@ export const Faucet = () => {
       console.error(e)
       setResult("Request unsuccessful")
     }
-
   }
 
   return (<>
-    <ReCAPTCHA
-      sitekey="6LdGFkMkAAAAAOaHxPwltZ7BxaFRfyzI-7e9XAW7"
-      onChange={setCaptcha}
-    />
-    <input type="text" value={address} onChange={(e) => setAddress(e.target.value)}/>
-    <button disabled={!captcha || !address} onClick={handleRequest}>Request</button>
-    <p>{result}</p>
+    <GoogleOAuthProvider clientId="505887002580-5tgcbuekhgscc0tbu92c5rtlpdp66o01.apps.googleusercontent.com">
+      <ReCAPTCHA
+        sitekey="6LdGFkMkAAAAAOaHxPwltZ7BxaFRfyzI-7e9XAW7"
+        onChange={setCaptcha}
+      />
+      {!googleAuthToken && <GoogleLogin onSuccess={({credential}) => setGoogleAuthToken(credential)} onError={() => setGoogleAuthToken(null)} />}
+      <input type="text" value={address} onChange={(e) => setAddress(e.target.value)}/>
+      <button disabled={!captcha || !address || !googleAuthToken} onClick={handleRequest}>Request</button>
+      <p>{result}</p>
+    </GoogleOAuthProvider>
   </>)
 }
 
