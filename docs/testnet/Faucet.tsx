d2d1cce79580389b2bf1aef37a8cfab31f01fedd
Sam Ruberti <samruberti@gmail.com>
2023-02-10 13:15:35 -0500

    Add /faucet page

--- docs/testnet/Faucet.tsx
+++ docs/testnet/Faucet.tsx
@@ -6,8 +6,8 @@ const FAUCET_URL = "https://ink-docs-rococo-faucet.parity-testnet.parity.io/drip
 
 export const Faucet = () => {
   const [captcha, setCaptcha] = useState<string | null>(null)
-  const [address, setAddress] = useState<string>("")
-  const [result, setResult] = useState<string>("")
+  const [address, setAddress] = useState("")
+  const [result, setResult] = useState("")
   const [inProgress, setInProgress] = useState(false)
 
   const handleRequest = async () => {
@@ -40,27 +40,42 @@ export const Faucet = () => {
     }
   }
 
-  return (<>
-    <ReCAPTCHA
-      sitekey={RECAPTCHA_SITE_KEY}
-      onChange={setCaptcha}
-    />
-    <label htmlFor="address-input">Address: </label>
-    <input
-      id="address-input"
-      style={{margin: 5, width: 400}}
-      type="text"
-      value={address}
-      placeholder="Address"
-      onChange={(e) => setAddress(e.target.value)}
-    />
-    <button
-      style={{margin: 5, padding: '5px 10px'}}
-      disabled={inProgress || !captcha || !address}
-      onClick={handleRequest}
-    >Request</button>
-    <p>{result}</p>
-  </>)
+  return (
+    <div className="faucetContainer">
+      <h3 className="faucetTitle">Get Testnet Tokens</h3>
+      <div className="faucetPlantContainer">
+        <img src="/img/plantOne.svg" alt="illustration of a sea grass plant" className="faucetPlantOne" />
+        <img src="/img/plantTwo.svg" alt="illustration of a sea plant" className="faucetPlantTwo" />
+      </div>
+      <div className="faucetHeroContainer">
+        <img src="/img/chest.svg" alt="image of a treasure chest" className="faucetHeroImage" />
+      </div>
+      <form className="withHero">
+        <fieldset>
+          <label htmlFor="address-input">Your SS58 Address</label>
+          <input
+            className="addressInput"
+            id="address-input"
+            type="text"
+            value={address}
+            placeholder="e.g. 5HprbfKUFdN4qfweVbgRtqDPHfNtoi8NoWPE45e5bD5AEKiR"
+            onChange={(e) => setAddress(e.target.value)}
+          />
+        </fieldset>
+        <ReCAPTCHA
+          sitekey={RECAPTCHA_SITE_KEY}
+          onChange={setCaptcha}
+        />
+        <button
+          disabled={inProgress || !captcha || !address}
+          onClick={handleRequest}
+        >
+          Request
+        </button>
+        {result && <p className="faucetResult">{result}</p>}
+      </form>
+    </div>
+  )
 }
 
 export default Faucet;
---
+++ docs/testnet/faucet.md
@@ -0,0 +1,9 @@
+---
+title: Faucet
+hide_title: true
+slug: /faucet
+---
+
+import Faucet from "./Faucet";
+
+<Faucet/>
