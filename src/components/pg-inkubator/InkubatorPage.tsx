31785d844f103ac9da7e10ed696d3753290f45c7
Sam Ruberti <samruberti@gmail.com>
2023-03-30 14:38:46 -0400

    feat: load metadata in SSR (#203)

--- src/components/pg-inkubator/InkubatorPage.tsx
+++ src/components/pg-inkubator/InkubatorPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { UIProvider, WindowProvider } from '../../contexts'
-import { Helmet } from 'react-helmet-async'
 import { Nav } from './Nav'
 import { SidebarMenu } from './SidebarMenu'
 import { Hero } from './Hero'
@@ -10,23 +9,10 @@ import { AuditorTrack } from './AuditorTrack'
 import { EcosystemGrants } from './EcosystemGrants'
 import { Footer } from './Footer'
 
-const TITLE = 'ink!ubator | Take your ink! project to new depths.'
-const DESCRIPTION =
-  'ink!ubator connects you with core developers and extensive resources. Get funding and technical support for your next ink! project.'
-
 const InkubatorPage = () => {
   return (
     <WindowProvider>
       <UIProvider>
-        <Helmet title={TITLE}>
-          <meta name="twitter:title" content={TITLE} />
-          <meta name="description" content={DESCRIPTION} />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:image" content="https://use.ink/img/twitter/inkubator-twitter.png" />
-          <meta name="twitter:description" content={DESCRIPTION} />
-          <meta name="twitter:site" content="@ink_lang" />
-          <meta name="twitter:creator" content="@ink_lang" />
-        </Helmet>
         <div className="font-montserrat bg-background-100 dark:bg-background-800 rounded-b-3xl">
           <div className="bg-light-background dark:bg-dark-background rounded-b-3xl xl:pb-12 pb-4 md:pt-40 pt-28">
             <Nav />
--- src/pages/ubator.tsx
+++ src/pages/ubator.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import BrowserOnly from '@docusaurus/BrowserOnly'
+import { Helmet } from 'react-helmet-async'
+
+const TITLE = 'ink!ubator | Take your ink! project to new depths.'
+const DESCRIPTION =
+  'ink!ubator connects you with core developers and extensive resources. Get funding and technical support for your next ink! project.'
 
 const Inkubator = () => (
-  <BrowserOnly>
-    {() => {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const InkubatorPage = require('../components/pg-inkubator').default
-      return <InkubatorPage />
-    }}
-  </BrowserOnly>
+  <>
+    <Helmet title={TITLE}>
+      <meta name="twitter:title" content={TITLE} />
+      <meta name="description" content={DESCRIPTION} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content="https://use.ink/img/twitter/inkubator-twitter.png" />
+      <meta name="twitter:description" content={DESCRIPTION} />
+      <meta name="twitter:site" content="@ink_lang" />
+      <meta name="twitter:creator" content="@ink_lang" />
+    </Helmet>
+    <BrowserOnly>
+      {() => {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const InkubatorPage = require('../components/pg-inkubator').default
+        return <InkubatorPage />
+      }}
+    </BrowserOnly>
+  </>
 )
 
 export default Inkubator
