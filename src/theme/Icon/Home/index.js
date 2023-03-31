89c4407f1b7cb1698d73b22d24a8e3242320f88e
peetzweg/ <phil.czek@gmail.com>
2023-03-31 21:57:56 +0200

    fix broken file from merge

--- src/theme/Icon/Home/index.js
+++ src/theme/Icon/Home/index.js
@@ -1,16 +1,16 @@
-
-import React from "react";
-import { useColorMode } from "@docusaurus/theme-common";
+import React from 'react'
+import { useColorMode } from '@docusaurus/theme-common'
 
 export default function IconHome(props) {
   return (
     <div
       style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       <img src="/img/lambo/breadcrumb.png" />
     </div>
-);
+  )
+}
