68dffa15086777b0ab84c5cb46d04c578b99d487
Dennis Zoma <3930150+wottpal@users.noreply.github.com>
2023-09-28 18:28:55 +0200

    Add ink!athon references & subpage (#278)
    * fix: Improve `dApp` casing consistency
    
    * feat: Improve `JSPackageHero`
    
    * feat: Improve frontend overview & Add ink!athon reference
    
    * feat: Add ink!athon subpage under third-party tools
    
    * feat: Update & fix links

--- docs/examples/dapps.md
+++ docs/examples/dapps.md
@@ -1,22 +1,22 @@
 ---
-title: Dapps
+title: DApps
 slug: /examples/dapps
 hide_title: true
 ---
 
 <img src="/img/title/balloons-2.svg" className="titlePic" />
 
-# Dapps
+# DApps
 
-We'll point to a couple full-stack Dapp examples here.
+We'll point to a couple full-stack dApp examples here.
 These can serve as inspiration for how to create a frontend for your
 smart contract.
 
 Three popular libraries for building frontends are:
 
-* [`useink`](/frontend/overview) an ergonomic React hooks library for interacting with Wasm contracts and Substrate.
-* [`polkadot-js/api`](https://github.com/polkadot-js/api) a low-level TypeScript API.
-* [useInkathon](https://github.com/scio-labs/use-inkathon) a React hooks library.
+- [`useink`](/frontend/overview) an ergonomic React hooks library for interacting with Wasm contracts and Substrate.
+- [`polkadot-js/api`](https://github.com/polkadot-js/api) a low-level TypeScript API.
+- [useInkathon](https://github.com/scio-labs/use-inkathon) a React hooks library.
 
 ## INK!athon
 
@@ -26,13 +26,13 @@ of an ink! smart contract and a React frontend using the
 
 ## link!
 
-link! is a URL shortener DApp. It consists of a frontend and an ink! contract.
+link! is a URL shortener dApp. It consists of a frontend and an ink! contract.
 
 link! uses [`polkadot-js/api`](https://github.com/polkadot-js/api) under the hood.
 The entire source code (contract + frontend) is located here:
 [https://github.com/paritytech/link](https://github.com/paritytech/link).
 
-You can view an online demo of the Dapp here: [https://tiny.ink](https://tiny.ink).
+You can view an online demo of the dApp here: [https://tiny.ink](https://tiny.ink).
 
 To create new short links you need `ROC` tokens from our testnet.
 See [here](/testnet) for how to get those.
--- src/components/JSPackageHero/JSPackageHero.tsx
+++ src/components/JSPackageHero/JSPackageHero.tsx
@@ -5,48 +5,59 @@ interface JSPackageHeroProps {
   title: string
   subtitle: string
   orgNameAndRepo: string // e.g. 'paritytech/useink'
-  packageName: string // e.g. 'useink'
+  packageName?: string // e.g. 'useink'
   logo?: string
+  chatUrl?: string
+  chatLogo?: string
 }
 
-export function JSPackageHero({ title, subtitle, packageName, orgNameAndRepo, logo }: JSPackageHeroProps) {
+export function JSPackageHero({ title, subtitle, packageName, orgNameAndRepo, logo, chatUrl, chatLogo }: JSPackageHeroProps) {
   return (
-    <header className="mb-10 flex flex-col items-center">
-      <div className="mt-8">
+    <header className="mb-4 flex flex-col items-center" id={title}>
+      <a href={`https://github.com/${orgNameAndRepo}`} target="_blank" rel="noreferrer" className="flex flex-col items-center !text-current hover:no-underline">
+      <div className="mt-12">
         <h1 className="sr-only">{title}</h1>
         {logo && <img src={logo} alt={title} className="w-28" />}
       </div>
 
       <h2 className="text-center text-lg mb-6 mt-0 md:!text-xl">{subtitle}</h2>
+      </a>
 
       <div className="flex flex-wrap gap-2 justify-center min-h-[3rem]">
-        <a aria-label="Version" href={`https://www.npmjs.com/package/${packageName}`} className="h-5">
+        {!!packageName && <a aria-label="Version" href={`https://www.npmjs.com/package/${packageName}`} className="h-5">
           <img
             alt={`${packageName} version`}
             src={`https://img.shields.io/npm/v/${packageName}?${toGithubLabel('Version')}`}
           />
-        </a>
+        </a>}
 
-        <a aria-label="License" href={`https://www.npmjs.com/package/${packageName}`} className="h-5">
-          <img
-            alt={`${packageName} license`}
-            src={`https://img.shields.io/github/license/${orgNameAndRepo}?${toGithubLabel('License')}`}
-          />
-        </a>
-
-        <a aria-label="Downloads" href={`https://www.npmjs.com/package/${packageName}`} className="h-5">
+        {!!packageName && <a aria-label="Downloads" href={`https://www.npmjs.com/package/${packageName}`} className="h-5">
           <img
             alt={`${packageName} downloads`}
             src={`https://img.shields.io/npm/dm/${packageName}?${toGithubLabel('Downloads')}`}
+          />
+        </a>}
+
+        <a aria-label="License" href={`https://github.com/${orgNameAndRepo}`} className="h-5">
+          <img
+            alt={`${orgNameAndRepo} license`}
+            src={`https://img.shields.io/github/license/${orgNameAndRepo}?${toGithubLabel('License')}`}
           />
         </a>
 
         <a aria-label="Stars" href={`https://github.com/${orgNameAndRepo}`} className="h-5">
           <img
-            alt={`${packageName} stars`}
+            alt={`${orgNameAndRepo} stars`}
             src={`https://img.shields.io/github/stars/${orgNameAndRepo}?${toGithubLabel('Stars')}`}
           />
         </a>
+
+        {!!chatUrl &&         <a aria-label="Chat" href={chatUrl} className="h-5">
+          <img
+            alt="Join the conversation"
+            src={`https://img.shields.io/badge/Join_the_conversation-_?color=e3e3e3&label=Chat${chatLogo ? `&logo=${chatLogo}&logoColor=e3e3e3` : ''}`}
+          />
+        </a>}
       </div>
     </header>
   )
