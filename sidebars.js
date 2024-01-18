498bcfbae474aa4867696f0e2f643a28b8ed352a
Michael Mueller <mich@elmueller.net>
2024-01-18 11:08:52 +0100

    Move linter content to 5.0 docs

--- sidebars.js
+++ sidebars.js
@@ -67,20 +67,6 @@ module.exports = {
       "datastructures/custom",
       "datastructures/storage-in-metadata",
     ],
-    "Linter": [
-      "linter/overview",
-      {
-        type: "category",
-        label: "Linting rules",
-        items: [
-            "linter/rules/no_main",
-            "linter/rules/primitive_topic",
-            "linter/rules/storage_never_freed",
-            "linter/rules/strict_balance_equality",
-            "linter/rules/non_fallible_api",
-        ]
-      }
-    ],
     "Frontend Development": [
       "frontend/overview",
       "frontend/getting-started",
