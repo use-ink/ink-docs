81ee52647205f3cfda5bda39dcc59c8f10514822
Niklas P <contact@niklasplessing.net>
2025-03-07 14:13:22 +0100

    links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,10 @@ export default function Home() {
       <h1 className="text-4xl font-bold">Hello ink!</h1>
       <p className="text-sm text-gray-500">(This will be the revamped ink! landing page)</p>
       <div className="flex flex-row gap-4">
-        <a href="/docs" className="text-purple-300 underline hover:text-purple-400">
+        <a href="docs" className="text-purple-300 underline hover:text-purple-400">
           docs →
         </a>
-        <a href="/ubator" className="text-purple-300 underline hover:text-purple-400">
+        <a href="ubator" className="text-purple-300 underline hover:text-purple-400">
           ink!ubator →
         </a>
       </div>
