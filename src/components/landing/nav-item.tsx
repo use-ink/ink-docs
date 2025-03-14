96ed7c82dfd2c847f88709f6e6b0f9952f8dcb64
Niklas P <contact@niklasplessing.net>
2025-03-14 12:45:56 +0100

    landing + sections

--- src/components/landing/nav-item.tsx
+++ src/components/landing/nav-item.tsx
@@ -3,6 +3,7 @@ import Link from '@docusaurus/Link'
 import { ChevronDown } from 'lucide-react'
 import { ListItem } from '../list-item'
 import { cn } from '../../util'
+import { CaretDown, Control } from '@phosphor-icons/react'
 
 interface NavItemProps {
   item: {
@@ -21,7 +22,7 @@ export function NavItem({ item }: NavItemProps) {
     return (
       <Link
         href={item.href}
-        className="flex items-center text-sm transition-colors text-foreground hover:text-primary font-[700] text-white"
+        className="flex items-center text-sm transition-colors text-foreground hover:text-primary font-[700] text-white hover:text-[#8c7cf7] hover:no-underline"
       >
         {item.title}
       </Link>
@@ -30,15 +31,19 @@ export function NavItem({ item }: NavItemProps) {
 
   return (
     <div className="relative group/nav-item">
-      <li className="flex items-center gap-1 font-[700] text-sm transition-colors outline-none cursor-pointer text-white group-hover/nav-item:text-primary">
+      <li className="flex items-center gap-1 font-[700] text-sm transition-colors outline-none cursor-pointer text-white group-hover/nav-item:text-primary hover:text-[#8c7cf7]">
         {item.title}
-        <ChevronDown className="w-4 h-4 transition-transform duration-200 group-hover/nav-item:rotate-180" />
+        <CaretDown
+          size={16}
+          weight="fill"
+          className="w-4 h-4 transition-transform duration-200 group-hover/nav-item:rotate-180"
+        />
       </li>
       <div
         className={cn(
           'absolute z-50 flex flex-col invisible w-[200px] gap-2 mt-2 transition-all duration-200 ease-in-out origin-top transform scale-y-90 scale-x-90 -translate-x-1/2 border shadow-md opacity-0 left-1/2 top-full bg-background',
           'group-hover/nav-item:visible group-hover/nav-item:opacity-100 group-hover/nav-item:scale-y-100 group-hover/nav-item:scale-x-100',
-          'bg-[#171233] p-[12px] rounded-[18px] shadow-[rgba(140,_124,_247,_0.12)_0px_2px_1px_0px_inset,_rgba(189,_130,_253,_0.15)_0px_0px_10px_0px_inset,_rgba(0,_0,_0,_0.24)_36px_36px_24px_0px]',
+          'bg-[#171233cc] backdrop-blur-md p-[12px] rounded-[18px] shadow-[rgba(140,_124,_247,_0.12)_0px_2px_1px_0px_inset,_rgba(189,_130,_253,_0.15)_0px_0px_10px_0px_inset,_rgba(0,_0,_0,_0.24)_36px_36px_24px_0px]',
         )}
       >
         {item.links.map((link) => (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,27 +8,53 @@ import { Button } from '../components/ui/button'
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <div className="flex flex-col items-center justify-center h-screen" style={{ fontFamily: 'Montserrat  ' }}>
-        <h1 className="text-4xl font-bold">Hello ink!</h1>
-        <p className="text-sm text-gray-500">(This will be the revamped ink! landing page)</p>
-        <div className="flex flex-row gap-4">
-          <Link to="/docs" className="">
-            <Button size="lg">docs →</Button>
-          </Link>
+      <Navbar className="z-20" />
+      <section className="relative h-screen section-intro">
+        <div className="absolute inset-0 z-0 section-bg"></div>
+        <div
+          className="relative z-10 flex flex-col items-center justify-center h-screen"
+          style={{ fontFamily: 'Montserrat  ' }}
+        >
+          <h1 className="text-4xl font-bold">Hello ink!</h1>
+          <p className="text-sm text-gray-500">(This will be the revamped ink! landing page)</p>
+          <div className="flex flex-row gap-4">
+            <Link to="/docs" className="">
+              <Button size="lg">docs →</Button>
+            </Link>
 
-          <Link to="/ubator" className="">
-            <Button
-              variant="secondary"
-              size="lg"
-              className="!transition-all duration-300 hover:scale-105 hover:-rotate-1"
-            >
-              ink!ubator
-            </Button>
-          </Link>
+            <Link to="/ubator" className="">
+              <Button
+                variant="secondary"
+                size="lg"
+                className="!transition-all duration-300 hover:scale-105 hover:-rotate-1"
+              >
+                ink!ubator
+              </Button>
+            </Link>
+          </div>
         </div>
-      </div>
-      <section className="h-screen"> </section>
+      </section>
+      <section className="h-screen section-interop">
+        <h2 className="text-4xl font-bold">Future-Proof Interoperability</h2>
+        <p className="text-sm text-gray-500">
+          Develop smart contracts that seamlessly integrate across Polkadot, Kusama, and Substrate.
+        </p>
+      </section>
+      <section className="h-screen section-rust"></section>
+      <section className="h-screen section-risc"></section>
+      <section className="relative h-screen section-composability">
+        <div className="absolute inset-0 h-full">
+          <img
+            decoding="async"
+            loading="lazy"
+            sizes="100vw"
+            srcSet="https://framerusercontent.com/images/QN8pq6Ny20mww2CWMxsJ7FI2w.png?scale-down-to=512 512w,https://framerusercontent.com/images/QN8pq6Ny20mww2CWMxsJ7FI2w.png?scale-down-to=1024 1024w,https://framerusercontent.com/images/QN8pq6Ny20mww2CWMxsJ7FI2w.png?scale-down-to=2048 2048w,https://framerusercontent.com/images/QN8pq6Ny20mww2CWMxsJ7FI2w.png 2200w"
+            src="https://framerusercontent.com/images/QN8pq6Ny20mww2CWMxsJ7FI2w.png?scale-down-to=2048"
+            className="object-cover w-full h-full"
+            alt="deep see"
+          />
+        </div>
+      </section>
       <Footer />
     </>
   )
