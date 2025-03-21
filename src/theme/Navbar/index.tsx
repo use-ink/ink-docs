12344fcfd73e469fc813f1b3258cd975c6701f5f
Niklas P <contact@niklasplessing.net>
2025-03-21 16:57:56 +0100

    docs nav

--- src/theme/Navbar/index.tsx
+++ src/theme/Navbar/index.tsx
@@ -1,22 +1,20 @@
 import React, { type ReactNode } from 'react'
 import NavbarLayout from '@theme/Navbar/Layout'
 import NavbarContent from '@theme/Navbar/Content'
-import { Navbar as MainNavbar } from '../../components/nav/nav'
-import NavbarSearch from './Search'
-import SearchBar from '@theme/SearchBar'
 
 export default function Navbar(): ReactNode {
   return (
-    // <NavbarLayout>
-    //   <NavbarContent />
-    // </NavbarLayout>
-    <MainNavbar
-      className="z-50"
-      childrenRight={
-        <NavbarSearch>
-          <SearchBar />
-        </NavbarSearch>
-      }
-    />
+    <NavbarLayout className="relative nav-top">
+      <div className="absolute inset-0 z-0 mask"></div>
+      <NavbarContent />
+    </NavbarLayout>
+    // <MainNavbar
+    //   className="z-50"
+    //   childrenRight={
+    //     <NavbarSearch>
+    //       <SearchBar />
+    //     </NavbarSearch>
+    //   }
+    // />
   )
 }
