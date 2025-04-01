4c68bae46c8c4ad9605815ab8e75e0404ddf9bc4
Niklas P <contact@niklasplessing.net>
2025-04-01 10:31:51 +0200

    docs

--- src/components/nav/nav-item.tsx
+++ src/components/nav/nav-item.tsx
@@ -16,9 +16,10 @@ interface NavItemProps {
     }[]
   }
   className?: string
+  dropdownClassName?: string
 }
 
-export function NavItem({ item, className }: NavItemProps) {
+export function NavItem({ item, className, dropdownClassName }: NavItemProps) {
   const baseUrl = useBaseUrl('/').slice(0, -1)
 
   if (!item.links?.length) {
@@ -47,6 +48,7 @@ export function NavItem({ item, className }: NavItemProps) {
           'absolute z-50 flex flex-col invisible w-[200px] gap-2 mt-2 transition-all duration-200 ease-in-out origin-top transform scale-y-90 scale-x-90 -translate-x-1/2 border shadow-md opacity-0 left-1/2 top-full bg-background',
           'group-hover/nav-item:visible group-hover/nav-item:opacity-100 group-hover/nav-item:scale-y-100 group-hover/nav-item:scale-x-100',
           'bg-[#171233cc] backdrop-blur-md p-[12px] rounded-[18px] shadow-[rgba(140,_124,_247,_0.12)_0px_2px_1px_0px_inset,_rgba(189,_130,_253,_0.15)_0px_0px_10px_0px_inset,_rgba(0,_0,_0,_0.24)_36px_36px_24px_0px]',
+          dropdownClassName,
         )}
       >
         {item.links.map((link) => {
