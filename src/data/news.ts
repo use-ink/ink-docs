b6b28d7d4ecbe046320788e5df300734e60bca27
Niklas P <contact@niklasplessing.net>
2025-07-03 11:29:45 +0200

    change description

--- src/data/news.ts
+++ src/data/news.ts
@@ -4,7 +4,7 @@ export const news = [
     date: '2025-07-03',
     title: 'ink! Project State Q1/25',
     description:
-      'In 2019, ink! began as a small initiative at Parity. Over the years, it evolved into a sophisticated ecosystem with a growing community around it. Throughout this time, Parity consistently allocated developer resources to ink!, which was truly remarkable...',
+      'An update on ink!, Polkadot’s Rust smart contract language — now led by the ink! Alliance, shifting from Wasm to RISC-V, with a roadmap focused on v6, ecosystem tools, and expanded developer adoption...',
     link: 'https://r0gue.medium.com/ink-project-state-q1-25-08441f4ac5d2',
   },
 ]
