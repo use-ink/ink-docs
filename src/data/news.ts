85f1ab29dcee2a4ca93a1ab83b7d34a2b513b7f8
Daanvdplas <daanvdplas@live.nl>
2025-09-11 11:58:36 +0800

    feat: blogposts

--- src/data/news.ts
+++ src/data/news.ts
@@ -1,10 +1,26 @@
 export const news = [
   {
+    image: '/img/title/solidity.svg',
+    date: '2025-08-27',
+    title: 'ink! speaks Solidity on PolkaVM',
+    description:
+      'With PolkaVM and pallet-revive on the horizon, ink! has learned to speak Solidity. This enables interoperability between ink! and Solidity contracts and...',
+    link: 'https://medium.com/coinsbench/ink-solidity-abi-on-polkavm-c675c854efd3',
+  },
+  {
+    image: '/img/title/web3-summit-hackathon.png',
+    date: '2025-08-15',
+    title: 'ink! at the Web3 Summit Hackathon',
+    description:
+      'ink! was present at the Web3 Summit Hackathon, showcasing the latest developments and engaging with the developer community. The hackathon provided an excellent platform to demonstrate ink!\'s capabilities and gather feedback from builders...',
+    link: 'https://medium.com/@r0gue/ink-at-the-web3summit-hackathon-8e9fd6ee0f79',
+  },
+  {
     image: '/img/title/monthly-update.svg',
     date: '2025-07-03',
     title: 'ink! Project State Q1/25',
     description:
-      'An update on ink!, Polkadot’s Rust smart contract language — now led by the ink! Alliance, shifting from Wasm to RISC-V, with a roadmap focused on v6, ecosystem tools, and expanded developer adoption...',
+      'An update on ink!, Polkadot\'s Rust smart contract language — now led by the ink! Alliance, shifting from Wasm to RISC-V, with a roadmap focused on v6, ecosystem tools, and expanded developer adoption...',
     link: 'https://r0gue.medium.com/ink-project-state-q1-25-08441f4ac5d2',
   },
 ]
