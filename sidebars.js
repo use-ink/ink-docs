645417fccc96bee3dbaabb6c6071e623412ce7fb
Michael Müller <mich@elmueller.net>
2025-11-28 01:08:41 +0100

    Add page comparing low-level Rust vs. ink! (#543)

--- sidebars.js
+++ sidebars.js
@@ -192,6 +192,7 @@ module.exports = {
       "background/polkadot-sdk",
       "background/why-rust",
       "background/why-riscv",
+      "background/lang-comparison",
       "background/precompiles",
       "background/ink-vs-solidity",
       "background/ink-vs-cosmwasm",
